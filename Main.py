bbfc1aa20aaafcb78c76c99f6e7134c45c317d99
ParoMatheus <42964672+ParoMatheus@users.noreply.github.com>
2018-09-11 11:13:29 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
 print("this is a recap")
 print("sooooooooo, this is cooool right?")
-print("Yo, look at me changing this shot")
+print("Yo, look at me changing this shit")
+print("Imma beat the %s out of %s"("shit","Nic")
